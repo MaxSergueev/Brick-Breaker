93f7e9fb2dd2c6bd32e23325c3016ea996af0f2b
Max Sergueev <max.sergueev@artfx.fr>
2024-09-18 10:38:48 +0200

    Scene Manager Implementation
    added a switch case to main.cpp to change between TITLE, GAMEPLAY and ENDING screens. No loss or victory conditions included, game state still persists even after entering ENDING screen

--- main.cpp
+++ main.cpp
@@ -8,42 +8,102 @@
 
 using namespace std;
 
+typedef enum GameScreen { TITLE, GAMEPLAY, ENDING } GameScreen;
+
 int main() {
 
     const float screenWidth = 600;
     const float screenHeight = 600;
 
+    GameScreen currentScreen = TITLE;
+
     Ball myBall(screenWidth / 3, screenHeight / 2, 5, 5, 5);
     Paddle myPaddle(screenWidth / 2, screenHeight * 7 / 8, screenWidth / 8, screenHeight / 40);
     BrickGrid brickGrid(screenWidth, screenHeight);
+
+    bool collision;
 
     InitWindow(screenWidth, screenHeight, "Brick Breaker");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        
-        myPaddle.Update(screenWidth);
-        myBall.Update();
 
-        bool collision = CheckCollisionCircleRec(myBall.circ, myBall.radius, myPaddle.vect);
+        switch (currentScreen)
+        {
+        case TITLE:
 
-        if (brickGrid.CheckCollision(myBall.circ, myBall.radius) || collision) {
-            myBall.speedY *= -1;
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                currentScreen = GAMEPLAY;
+            }
+
+            break;
+
+        case GAMEPLAY:
+
+            myPaddle.Update(screenWidth);
+            myBall.Update();
+
+            myBall.CheckWindowCollision(screenWidth, screenHeight);
+            collision = CheckCollisionCircleRec(myBall.circ, myBall.radius, myPaddle.vect);
+
+            if (brickGrid.CheckCollision(myBall.circ, myBall.radius) || collision) {
+                myBall.speedY *= -1;
+            }
+
+            if (myBall.circ.y > screenHeight)
+            {
+                currentScreen = ENDING;
+            }
+
+            break;
+
+        case ENDING:
+
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                currentScreen = TITLE;
+            }
+
+            break;
+
+        default: break;
         }
 
-
         BeginDrawing();
+
         ClearBackground(DARKPURPLE);
 
-        myPaddle.Draw();
-        myBall.Draw();
-        brickGrid.Draw();
+        switch (currentScreen)
+        {
+        case TITLE:
+        {
+         
+            DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+            DrawText("BRICK BREAKER", screenWidth / 6, screenHeight * 0.36, 40, DARKGREEN);
+            DrawText("PRESS ENTER", screenWidth / 6, screenHeight * 0.43, 20, DARKGREEN);
 
-        DrawText(TextFormat("Score: %i", brickGrid.Score()), screenWidth * 0.05, screenHeight * 0.95, 20, RED);
+        } break;
+        case GAMEPLAY:
+        {
+            myPaddle.Draw();
+            myBall.Draw();
+            brickGrid.Draw();
+
+            DrawText(TextFormat("Score: %i", brickGrid.Score()), screenWidth * 0.05, screenHeight * 0.95, 20, RED);
+
+        } break;
+        case ENDING:
+        {
+            DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+            DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+            DrawText("PRESS ENTER", 120, 220, 20, DARKBLUE);
+
+        } break;
+        default: break;
+        }
 
         EndDrawing();
-
-        myBall.CheckWindowCollision(screenWidth, screenHeight);
 
         
     }
